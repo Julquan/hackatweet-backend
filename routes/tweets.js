67331a3d736622da8ec7e50434baf3748bebf826
JulienQ <jquan84@gmail.com>
2022-11-24 16:53:56 +0100

    route get for search hashtag

--- routes/tweets.js
+++ routes/tweets.js
@@ -3,18 +3,18 @@ var router = express.Router();
 const Tweet = require('../models/tweets')
 
 //create / store tweet to database and return all tweets available
-router.post('/tweet', (req, res) => {
+router.post('/post', (req, res) => {
 
     //to be modified: firstname and username from user token '/tweet/:token'
 
     let { firstname, username, content } = req.body;
 
-    if(!content) {
-        res.json({result: false, message: 'No tweet submitted.'});
+    if (!content) {
+        res.json({ result: false, message: 'No tweet submitted.' });
         return;
     }
 
-    Tweet.find().then(data =>{
+    Tweet.find().then(data => {
         const pattern = /#.\S*/gi;
         const newTweet = new Tweet({
             firstname,
@@ -28,8 +28,8 @@ router.post('/tweet', (req, res) => {
 
             Tweet.find().then(tweets => {
                 res.json({
-                    result: true, 
-                    message: 'New tweet created.', 
+                    result: true,
+                    message: 'New tweet created.',
                     tweets,
                 });
             });
@@ -38,8 +38,17 @@ router.post('/tweet', (req, res) => {
 });
 
 //get tweets with an specific hashtag
-router.get('/hashtag', (req, res) => {
+    router.get('/search/:hashtag', (req, res) => { 
+        const {hashtag} = req.params; 
+        // console.log(hashtag)
+        Tweet.find({ hashtag: { $all: [`${hashtag}`]}}).then( data => { 
+            res.json(data) }
+        )    
+    }) 
+        
+    
 
-});
+
+
 
 module.exports = router;
